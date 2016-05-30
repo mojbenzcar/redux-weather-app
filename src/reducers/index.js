7e16ece2e4caafdf7fd8bc25667447f5c1e995a4
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2016-05-30 15:36:30 +0545

    weather app init

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
+
+import BooksReducer from './books';
+import ActiveBook from './active-book';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+  books: BooksReducer,
+  activeBook: ActiveBook
+})
 
 export default rootReducer;
